2676a28a983de9f46c6cf369468759710e8b52e0
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-01-15 14:16:08 -0300

    finished footer components

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import './styles.css'
 const Footer = () => {
     return (
-        <div>
-            This the footer
+        <div className="footer-container">
+            <div className="footer-container-left">
+                <h4> © Copyrights 2019. All Rigths Reserved</h4>
+            </div> 
+            <div className="footer-container-right">
+                <h4>Privacy Policy</h4>
+                <h4>Terms and Conditions</h4>
+            </div>
         </div>
     )
 }
