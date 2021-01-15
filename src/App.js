cfc3ecb086b440141f8d7a0b68f3052e1a9d49c3
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-01-15 12:07:27 -0300

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { Switch, Route } from "react-router-dom";
+import Error from './pages/Error'
+import Landing from './pages/Landing'
+import Register from './pages/Register'
 const App = () => {
   return (
-    <div>
-      Test
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/register" component={Register} />
+        <Route component={Error} />
+      </Switch>
+      <Footer />
+    </>
+  );
+};
 
-export default App
-
+export default App;
---
+++ src/pages/Register.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register = () => {
+    return (
+        <div>
+            This is the Register Page
+        </div>
+    )
+}
+
+export default Register
