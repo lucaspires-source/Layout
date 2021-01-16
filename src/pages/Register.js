ed97dc555a62426f1bee92729f9a3119e951f99b
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-01-16 10:58:08 -0300

    created a custom hook to use local storage

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,26 +1,34 @@
-import React from 'react'
-
+import React from "react";
+import { Formik } from "formik";
 const Register = () => {
+  
+ 
+  
     return (
-        <form>
-            <div>
-                <label>Nome</label>
-                <input/>
-            </div>
-            <div>
-                <label>Nascimento</label>
-                <input/>
-            </div>
-            <div>
-                <label>CPF</label>
-                <input/>
-            </div>
-            <div>
-                <label>CEP</label>
-                <input/>
-            </div>     
-        </form>
-    )
-}
 
-export default Register
+    <Formik
+      render={() => (
+        <form>
+          <div>
+            <label>Nome</label>
+            <input />
+          </div>
+          <div>
+            <label>Nascimento</label>
+            <input />
+          </div>
+          <div>
+            <label>CPF</label>
+            <input />
+          </div>
+          <div>
+            <label>CEP</label>
+            <input />
+          </div>
+        </form>
+      )}
+    />
+  );
+};
+
+export default Register;
